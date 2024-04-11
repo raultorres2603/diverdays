72c7212ebe9e3881ef0e9e0db0cba39615a1a5cd
Raul Torres <raultorres2603@gmail.com>
2024-04-11 17:52:06 +0200

    profile doing

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -16,9 +16,9 @@ export const Profile = () => {
     <div>
       <div className="grid grid-cols-2">
         {" "}
-        <div className="w-32 h-32 bg-zinc-400 rounded-full">
+        <div className="w-36 h-36 bg-sky-300 rounded-full">
           {!user ? (
-            <Skeleton className="w-32 h-32 bg-zinc-400 rounded-full" />
+            <Skeleton className="w-36 h-36 bg-zinc-400 dark:bg-zinc-400 rounded-full" />
           ) : !user.avatar ? (
             <UserCircleIcon />
           ) : (
@@ -27,19 +27,38 @@ export const Profile = () => {
         </div>
         <div>
           {!user ? (
-            <Skeleton className="text-xl bg-zinc-400" count={1} />
+            <Skeleton className="text-xl bg-zinc-400 my-3" count={3} />
           ) : (
-            <input
-              className={`form-control text-2xl w-full text-center ${
-                !user.name && "border-2 border-cyan-100 rounded-lg"
-              }`}
-              defaultValue={user.name}
-              placeholder="Nombre"
-            />
+            <div className="grid grid-rows-3 gap-4">
+              <input
+                className={`form-control border-4 text-2xl w-full text-center rounded-lg ${
+                  !user.name ? "border-red-300" : "border-sky-300"
+                }`}
+                defaultValue={user.name}
+                placeholder="Nombre"
+              />
+              <input
+                className={`form-control border-4 text-2xl w-full text-center rounded-lg ${
+                  !user.fname ? "border-red-300" : "border-sky-300"
+                }`}
+                defaultValue={user.fname}
+                placeholder="Apellido"
+              />
+              <select
+                className={`form-select rounded-lg border-4 text-2xl w-full text-center ${
+                  !user.genre ? "border-red-300" : "border-sky-300"
+                }`}
+                defaultValue={user.genre ? user.genre : "N"}
+              >
+                <option value="N">Selecciona</option>
+                <option value="H">Hombre</option>
+                <option value="M">Mujer</option>
+              </select>
+            </div>
           )}
         </div>
       </div>
-
+      {/* Footer */}
       <ArrowLeftEndOnRectangleIcon
         stroke="red"
         onClick={() => {
