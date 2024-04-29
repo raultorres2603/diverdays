043294e960d3243543652b6e8f6a8965fe477f85
Raul Torres <raultorres2603@gmail.com>
2024-04-29 09:14:48 +0200

    diverAdd view

--- src/components/DiverCalendar.tsx
+++ src/components/DiverCalendar.tsx
@@ -1,7 +1,10 @@
 import { useContext, Fragment, useRef, useState } from "react";
 import { mContext } from "../contexts/MainContext";
 import User from "../classes/User";
-import { ArrowLeftEndOnRectangleIcon } from "@heroicons/react/24/outline";
+import {
+  ArrowLeftEndOnRectangleIcon,
+  PlusIcon,
+} from "@heroicons/react/24/outline";
 import { BackwardIcon } from "@heroicons/react/24/outline";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
@@ -17,6 +20,10 @@ export const DiverCalendar = () => {
   const [diverday, setDiverDay] = useState(0);
 
   const [openDiverDaySet, setOpenDiverDaySet] = useState(false);
+  const [openCelebDiver, setOpenCelebDiver] = useState(false);
+
+  const [selectedDiver, setSelectedDiver] = useState(0);
+  const [diverPhotos, setDiverPhotos] = useState<string[]>([]);
 
   const cancelButtonRef = useRef(null);
 
@@ -102,6 +109,10 @@ export const DiverCalendar = () => {
                       ? "bg-sky-800 dark:bg-sky-600 hover:shadow-sky-300/50 active:bg-sky-500 dark:active:bg-sky-700 active:shadow-sky-300/50"
                       : "bg-lime-600 dark:bg-lime-500 hover:shadow-lime-300/50 active:bg-lime-500 dark:active:bg-lime-700 active:shadow-lime-300/50"
                   } rounded-lg hover:scale-105 hover:shadow-lg hover:border-5 active:shadow-xl active:border-5 active:scale-90 select-none`}
+                  onClick={() => {
+                    setSelectedDiver(diverday);
+                    setOpenCelebDiver(true);
+                  }}
                 >
                   {diverday}
                 </div>
@@ -200,6 +211,106 @@ export const DiverCalendar = () => {
                       type="button"
                       className="transition-all ease-in-out mt-3 inline-flex bg-red-500 hover:bg-red-600 hover:scale-105 w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                       onClick={() => setOpenDiverDaySet(false)}
+                      ref={cancelButtonRef}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+
+      {/* Modal para celebrar un diversario */}
+      <Transition.Root show={openCelebDiver} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setOpenCelebDiver}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                  <div className="dark:bg-slate-800 bg-sky-900 text-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4 justify-center flex">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                        <Dialog.Title
+                          as="h3"
+                          className="text-white font-semibold leading-6"
+                        >
+                          Celebra tu diversario
+                        </Dialog.Title>
+                        <div className="mt-2">
+                          <p className="text-sm text-white">
+                            Actualiza tu diversario
+                          </p>
+                        </div>
+                        <div className="mt-2 text-sm text-red-600 dark:text-red-400 font-bold">
+                          {comprobDiverDay(selectedDiver) == "past" &&
+                            "No puedes celebrar un diversario pasado"}
+                          {comprobDiverDay(selectedDiver) == "tomorrow" &&
+                            "No puedes celebrar un diversario que aún no ha pasado"}
+                          {comprobDiverDay(selectedDiver) == "today" && (
+                            <>
+                              <div className="inputFiles mt-4">
+                                <input
+                                  type="file"
+                                  id="diverFoto"
+                                  className="hidden"
+                                />
+                                <div className="transition-all ease-in-out inputBox w-20 h-20 border-dashed border-4 border-sky-200 mx-auto hover:cursor-pointer hover:border-sky-300 hover:scale-105">
+                                  <div className="plusMark">
+                                    <PlusIcon
+                                      color="lightblue"
+                                      className="transition-all ease-in-out w-18 my-auto mx-auto hover:scale-105"
+                                    />
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="comments mt-4">
+                                <input
+                                  type="text"
+                                  id="diverComment"
+                                  placeholder="Introduce un comentario"
+                                  className="rounded-lg text-sm text-black dark:text-white px-3 py-2 bg-slate-200 dark:bg-slate-600 border border-slate-300 dark:border-slate-500"
+                                />
+                              </div>
+                            </>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="dark:bg-slate-700 bg-sky-600 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button
+                      type="button"
+                      className="transition-all ease-in-out inline-flex w-full hover:scale-105 justify-center rounded-md bg-lime-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-lime-700 sm:ml-3 sm:w-auto"
+                    >
+                      Añadir
+                    </button>
+                    <button
+                      type="button"
+                      className="transition-all ease-in-out mt-3 inline-flex bg-red-500 hover:bg-red-600 hover:scale-105 w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                      onClick={() => setOpenCelebDiver(false)}
                       ref={cancelButtonRef}
                     >
                       Cancel
